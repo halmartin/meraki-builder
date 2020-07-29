ab7463a3646e0b4a0f80e377142642ab00886dcf
Hal Martin <1230969+halmartin@users.noreply.github.com>
2020-07-29 17:19:13 +0000

    Don't require the dump to be named mtd12-original.dat, use python if 'find_hdr' is not in /usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/home/hmartin/bin

--- stage2/extract.sh
+++ stage2/extract.sh
@@ -2,15 +2,26 @@
 
 DUMP_FILE=${1}
 
+if [ "$DUMP_FILE" == "" ]; then
+  DUMP_FILE=mtd12-original.dat
+fi
+
 test -d bin || mkdir bin
 
-RAMDISK_OFFSET=$(find_hdr -g mtd12-original.dat)
+command -v find_hdr
+if [ $? -ne 0 ]; then
+  # use python
+  RAMDISK_OFFSET=$(python3 ../tools/find_hdr.py -g $DUMP_FILE)
+else
+  # someone compiled my garbage C!
+  RAMDISK_OFFSET=$(find_hdr -g $DUMP_FILE)
+fi
 # Extract mtd12 or mtd13's ramdisk
-dd if=mtd12-original.dat of=bin/mtd12-header  bs=1024 count=1
-dd if=mtd12-original.dat of=bin/mtd12-vmlinux bs=1 skip=1024 count=$((RAMDISK_OFFSET-1024))
-dd if=mtd12-original.dat of=ramdisk.gz     bs=1 skip=$((RAMDISK_OFFSET))
+dd if=$DUMP_FILE of=bin/mtd12-header  bs=1024 count=1
+dd if=$DUMP_FILE of=bin/mtd12-vmlinux bs=1 skip=1024 count=$((RAMDISK_OFFSET-1024))
+dd if=$DUMP_FILE of=ramdisk.gz     bs=1 skip=$((RAMDISK_OFFSET))
 # Yeah, I'm not convinced of Meraki's instructions that rootlist is empty, given the footer contents
-#dd if=mtd12-original.dat of=bin/mtd12-post bs=1 count=$((0xf70)) skip=$((0x04af758 + 0xc626e2))
+#dd if=$DUMP_FILE of=bin/mtd12-post bs=1 count=$((0xf70)) skip=$((0x04af758 + 0xc626e2))
 
 # Extract the ramdisk
 test -d ramdisk && rm -rf ramdisk

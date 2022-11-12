c49befcc3905c2b74000226f728e71e50b3286da
Hal Martin <1230969+halmartin@users.noreply.github.com>
2022-11-12 20:15:53 +0100

    Generate lsb-release in the image

--- buildroot/board/meraki/ms220/post-build.sh
+++ buildroot/board/meraki/ms220/post-build.sh
@@ -12,3 +12,12 @@ if [ -h ${TARGET}/etc/dropbear ]; then
     rm ${TARGET}/etc/dropbear
     echo "Removed /etc/dropbear symlink"
 fi
+
+# set the lsb-release
+IMAGE_DATE=$(date +%Y%m%d)
+mkdir -p ${TARGET}/etc
+cat << EOF > ${TARGET}/etc/lsb-release
+DISTRIB_ID="postmerkOS"
+DISTRIB_RELEASE="$IMAGE_DATE"
+DISTRIB_DESCRIPTION="postmerkOS mipsel"
+EOF

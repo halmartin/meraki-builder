20fd5132f14aa6de6668115a02baa9073c267745
Hal Martin <1230969+halmartin@users.noreply.github.com>
2023-04-21 09:17:24 +0200

    MS220: post-build fixups

--- buildroot/board/meraki/ms220/post-build.sh
+++ buildroot/board/meraki/ms220/post-build.sh
@@ -1,23 +1,28 @@
 #!/bin/sh
 
-TARGET=$1
-
-mkdir -p ${TARGET}/overlay
+mkdir -p ${TARGET_DIR}/overlay
 echo "Created /overlay"
-mkdir -p ${TARGET}/click
+mkdir -p ${TARGET_DIR}/click
 echo "Created /click"
 
 # remove dropbear, it will be created on JFFS2 overlay
-if [ -h ${TARGET}/etc/dropbear ]; then
-    rm ${TARGET}/etc/dropbear
+if [ -h ${TARGET_DIR}/etc/dropbear ]; then
+    rm ${TARGET_DIR}/etc/dropbear
     echo "Removed /etc/dropbear symlink"
 fi
 
 # set the lsb-release
 IMAGE_DATE=$(date +%Y%m%d)
-mkdir -p ${TARGET}/etc
-cat << EOF > ${TARGET}/etc/lsb-release
+mkdir -p ${TARGET_DIR}/etc
+cat << EOF > ${TARGET_DIR}/etc/lsb-release
 DISTRIB_ID="postmerkOS"
 DISTRIB_RELEASE="$IMAGE_DATE"
 DISTRIB_DESCRIPTION="postmerkOS mipsel"
 EOF
+
+# get the salt from /etc/shadow and replace it in the initscript
+salt=$(grep -E '^root' ${TARGET_DIR}/etc/shadow | awk -F'$' '{print $3}')
+if [ -f ${TARGET_DIR}/etc/init.d/S14passwd ]; then
+    sed -i "s/__SALT__/${salt}/g" ${TARGET_DIR}/etc/init.d/S14passwd
+    echo "Changed salt to \"${salt}\" in /etc/init.d/S14passwd"
+fi
